a6e75d1a42f989add9b77c9b6543267f32097443
Chloe Gip <chloeGip@student.unsw.edu.au>
2025-11-12 21:39:27 +1100

    added register page and register user functionality

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,12 +1,80 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 import styles from '../css/Auth.module.css';
 
 function Register() {
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ confirmPassword, setConfirmPassword ] = useState('');
+  const [ name, setName ] = useState('');
+
+  const navigate = useNavigate();
+  
+  const registerUser = async () => {
+    console.log(email, password, confirmPassword, name);
+
+    try {
+      const response = await axios.post('http://localhost:5005/user/auth/register', { email, password, name });
+      localStorage.setItem('token', response.data.token);
+      navigate('/dashboard');
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
   return (
     <div>
-      Register
+      <h1>Register</h1><br />
+      <TextField
+        id="register-email-input"
+        label="Email"
+        type="email"
+        variant="outlined"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <br />
+      <TextField
+        id="register-password-input"
+        label="Password"
+        type="password"
+        variant="outlined"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <br />
+      <TextField
+        id="register-confirm-password-input"
+        label="Confirm Password"
+        type="password"
+        variant="outlined"
+        value={confirmPassword}
+        onChange={e => setConfirmPassword(e.target.value)}
+      />
+      <br />
+      <TextField
+        id="register-name-input"
+        label="Name"
+        type="email"
+        variant="outlined"
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <br />
+      <Button
+        variant="contained"
+        onClick={registerUser}
+      >
+        Register
+      </Button>
     </div>
   )
 }
 
-export default Register
+export default Register;
