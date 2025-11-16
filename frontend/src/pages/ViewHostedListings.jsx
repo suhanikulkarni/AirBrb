4e7bb2255b7cd3fdce624d44dd9548f49283907b
Suhani Kulkarni <z5585741@ad.unsw.edu.au>
2025-11-16 20:45:26 +1100

    f1: created a screen of listings the user has created. I have not finished the display, will do whem the create is implemented

--- frontend/src/pages/ViewHostedListings.jsx
+++ frontend/src/pages/ViewHostedListings.jsx
@@ -6,17 +6,13 @@ const getAllListings = async (owner) => {
   let hostedListings = [];
   try {
     const res = await axios.get(`${API_BASE_URL}listings`);
-    console.log("bfbnfj",res)
     if (res) {
       const listingData = res.data.listings;
-      console.log("listingData", listingData)
       listingData.forEach(element => {
         if (element.owner === owner) {
-          hostedListings.push(element)
+          hostedListings.push(element.id)
         }
       });
-        
-      console.log("hosted listings",hostedListings);
       return hostedListings;
     } 
   }
@@ -25,30 +21,60 @@ const getAllListings = async (owner) => {
   }
 }
 
+const getListingInfo = async (id) => {
+  try {
+    const res = await axios.get(`${API_BASE_URL}listings/${id}`);
+    if (res) return res.data.listing;
+    
+  }
+  catch {
+    console.log("ERRORROR");
+    return;
+  }
+}
+
 function viewHostedListings({ owner }) {
-  const [listings, setListings] = useState([{}]);
+  const [listings, setListings] = useState([]);
   
   useEffect (() => {
 
     const getFetch = async () => {
-      const res = await getAllListings(owner);
-      console.log(res)
-      if (res) setListings(res);
-      console.log(listings)
+      try {
+
+        const listingIds = await getAllListings(owner);
+        if (!listingIds || listingIds.length === 0) {
+          setListings([]);
+          return;
+        }
+
+        const detailedListings = await Promise.all(
+          listingIds.map(id => getListingInfo(id))
+        );
+        setListings(detailedListings.filter(Boolean));
+
+      }
+      catch (error) {
+        console.error("Error fetching listings:", error);
+      }
     } 
     getFetch(owner)
-  }, [owner]);
+        
+
+  }, []);
+
+  console.log("listigns", listings)
   return (
     <div>
       <h2>Hosted Listings</h2>
       {listings.length === 0 ? (
-        <p>No listings found for owner: {owner}</p>
+        <p></p>
       ) : (
         listings.map((listing) => (
-          <div key={listing.id}>
+          <div key={listing.title}>
             <h3>{listing.title}</h3>
-            <p>{listing.address?.value}</p>
             <p>${listing.price}</p>
+            <p>Number of Bedrooms: Need to change to beds: {listing.metadata.bedrooms}</p>
+            <p>{listings.price}</p>
           </div>
         ))
       )}
