065445e5eee94a64ba1a960c362cd706276c8cca
Chloe Gip <chloeGip@student.unsw.edu.au>
2025-11-19 21:44:51 +1100

    fixed conflicts

--- frontend/src/helper.jsx
+++ frontend/src/helper.jsx
@@ -14,18 +14,18 @@
  * @return {Promise<string>} Promise which resolves to the file as a data url.
  */
 export function fileToDataUrl(file) {
-    const validFileTypes = [ 'image/jpeg', 'image/png', 'image/jpg' ]
-    const valid = validFileTypes.find(type => type === file.type);
-    // Bad data, let's walk away.
-    if (!valid) {
-        throw Error('provided file is not a png, jpg or jpeg image.');
-    }
+  const validFileTypes = [ 'image/jpeg', 'image/png', 'image/jpg' ]
+  const valid = validFileTypes.find(type => type === file.type);
+  // Bad data, let's walk away.
+  if (!valid) {
+    throw Error('provided file is not a png, jpg or jpeg image.');
+  }
     
-    const reader = new FileReader();
-    const dataUrlPromise = new Promise((resolve,reject) => {
-        reader.onerror = reject;
-        reader.onload = () => resolve(reader.result);
-    });
-    reader.readAsDataURL(file);
-    return dataUrlPromise;
+  const reader = new FileReader();
+  const dataUrlPromise = new Promise((resolve,reject) => {
+    reader.onerror = reject;
+    reader.onload = () => resolve(reader.result);
+  });
+  reader.readAsDataURL(file);
+  return dataUrlPromise;
 }
