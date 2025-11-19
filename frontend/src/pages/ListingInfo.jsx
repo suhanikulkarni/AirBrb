f48170a35e9081d1aac2e17ec13602c1678311c5
Suhani Kulkarni <z5585741@ad.unsw.edu.au>
2025-11-19 17:37:58 +1100

    f4.4 when you click on the rating, the comments with that rating appear

--- frontend/src/pages/ListingInfo.jsx
+++ frontend/src/pages/ListingInfo.jsx
@@ -3,7 +3,9 @@ import { useParams } from 'react-router-dom';
 import { API_BASE_URL } from '../constants';
 import axios from 'axios';
 import UsersBookingForm from './UserBookingForm';
-import { Box, TextField } from '@mui/material';
+import { Box, Modal, TextField, Tooltip } from '@mui/material';
+
+import Rating from '@mui/material/Rating';
 
 const getListingInfo = async (id) => {
   try {
@@ -22,7 +24,22 @@ const getListingInfo = async (id) => {
 
 function ListingInfo({ token }) {
   const { id } = useParams();
+  const [open, setOpen] = useState(false);
   const [listingDetails, setListingDetails] = useState(null);
+
+  const [reviewValue, setReviewValue] = useState(0);
+  const [specificRatingReviews, setSpecificRatingReviews] = useState([])
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    console.log("Opennign")
+    setOpen(true);
+  };
+
+
 
   useEffect(() => {
     const fetchListings = async () => {
@@ -37,9 +54,29 @@ function ListingInfo({ token }) {
     sum = sum+element.bedCount;
   });
 
+  const filterReviews = () => {
+
+    if (listingDetails) {
+    // console.log(listingDetails)
+    // console.log("console.log",listingDetails.reviews)
+
+    const value = listingDetails.reviews.filter((review) => review.rating === reviewValue);
+    console.log("plk",value)
+
+    setSpecificRatingReviews(value);
+}
+  }
+
+  useEffect(() => {
+    filterReviews()
+
+  },[reviewValue, listingDetails])
+
+
   return (
     <>
       {listingDetails && (
+        <>
         <div>
           <h2>Listing Info</h2>
           <p>Title: {listingDetails?.title}</p>
@@ -55,11 +92,17 @@ function ListingInfo({ token }) {
           <p>Amenities: {listingDetails?.metadata?.amenities}</p>
           <p>Price: ${listingDetails?.price} per night</p>
           <p>Property Type: {listingDetails?.metadata?.propertyType}</p>
-          {/* <p>Reviews: {listingDetails?.reviews}</p> */}
           <p>Number of Beds: {listingDetails?.metadata?.bedroomCount}</p>
           <p>Number of Bathrooms: {listingDetails?.metadata?.bathroomCount}</p>
           <p>Number of Beds: {sum}</p>
         </div>
+
+        <Tooltip title='reviewValue'>
+          <Rating value={reviewValue}
+            onClick={handleOpen}
+            onChange={(event, newValue) => setReviewValue(newValue)}></Rating>
+        </Tooltip>
+        </>
       )}
 
       {listingDetails && token && token !== 'LOADING' && (
@@ -69,6 +112,35 @@ function ListingInfo({ token }) {
           token={token}
         />
       )}
+
+      <Modal
+          open={open}
+          onClose={handleClose}
+        >
+        <Box sx={{ 
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: 400,
+          bgcolor: 'background.paper',
+          border: '2px solid #000',
+          boxShadow: 24,
+          p: 4,
+        }}>
+          <h2>Reviews with {reviewValue} stars</h2>
+
+          {specificRatingReviews.map((review) => {
+            return(<div>
+              <p>{review.comment}</p>
+
+            </div>)
+          })}
+
+          <button onClick={handleClose}>Close</button>
+        </Box>
+      </Modal>
+     
     </>
   );
 }
