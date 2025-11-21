88938fbaa58b7199b6d7c6ac4fdeb7d7aa5e9b8e
Chloe Gip <chloeGip@student.unsw.edu.au>
2025-11-21 19:46:42 +1100

    fix code style

--- frontend/src/pages/BedroomForm.jsx
+++ frontend/src/pages/BedroomForm.jsx
@@ -33,8 +33,7 @@ function BedroomForm({ bedroomNumber, updateBedroomMetadata, bedroomMetadata }) 
       bedCount: totalBeds
     });
   }
-    
-  // TODO: set bed info into listing info
+  
   return (
     <>
       <h4>Bedroom {bedroomNumber}</h4>
@@ -47,47 +46,47 @@ function BedroomForm({ bedroomNumber, updateBedroomMetadata, bedroomMetadata }) 
         }}
       >
         <TextField
-          label="Single Beds"
-          type="number"
+          label='Single Beds'
+          type='number'
           value={bedroomInfo.singleBed}
           onChange={handleBedroomBedInfo}
-          name="singleBed"
+          name='singleBed'
           slotProps={{ input: { min: 0 } }}
         />
 
         <TextField
-          label="Double Beds"
-          type="number"
+          label='Double Beds'
+          type='number'
           value={bedroomInfo.doubleBed}
           onChange={handleBedroomBedInfo}
-          name="doubleBed"
+          name='doubleBed'
           slotProps={{ input: { min: 0 } }}
         />
 
         <TextField
-          label="Queen Beds"
-          type="number"
+          label='Queen Beds'
+          type='number'
           value={bedroomInfo.queenBed}
           onChange={handleBedroomBedInfo}
-          name="queenBed"
+          name='queenBed'
           slotProps={{ input: { min: 0 } }}
         />
 
         <TextField
-          label="King Beds"
-          type="number"
+          label='King Beds'
+          type='number'
           value={bedroomInfo.kingBed}
           onChange={handleBedroomBedInfo}
-          name="kingBed"
+          name='kingBed'
           slotProps={{ input: { min: 0 } }}
         />
 
         <TextField
-          label="Sofa Beds"
-          type="number"
+          label='Sofa Beds'
+          type='number'
           value={bedroomInfo.sofaBed}
           onChange={handleBedroomBedInfo}
-          name="sofaBed"
+          name='sofaBed'
           slotProps={{ input: { min: 0 } }}
         />
       </Box>
--- frontend/src/pages/CreateListing.jsx
+++ frontend/src/pages/CreateListing.jsx
@@ -1,18 +1,15 @@
 import { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
-import { API_BASE_URL, DEFAULT_IMAGE } from '../constants';
 import { useNavigate } from 'react-router-dom';
-
+import { API_BASE_URL, DEFAULT_IMAGE } from '../constants';
+import { ErrorContext } from '../context';
 import { Form, PageBody } from '../styles/mainStyles';
 import Button from '@mui/material/Button';
-
-import { ErrorContext } from '../context';
-
-
 import ListingForm from './ListingForm';
 
 function CreateListing({ token }) {
   const navigate = useNavigate();
+
   useEffect(() => {
     if (token === 'LOADING' || !token) navigate('/login');
   }, [token]);
@@ -48,14 +45,14 @@ function CreateListing({ token }) {
     listingAddress,
     listingMetadata,
     thumbnailType
-  }
+  };
 
   const setters = {
     setListingInfo,
     setListingAddress,
     setListingMetadata,
     setThumbnailType
-  }
+  };
 
   const setShowErrorPopup = useContext(ErrorContext);
   
@@ -73,48 +70,47 @@ function CreateListing({ token }) {
   }
 
   useEffect(() => {
-    console.log("Updated address:", listingAddress);
+    console.log('Updated address:', listingAddress);
   }, [listingAddress]);
 
   useEffect(() => {
-    console.log("Updated metadata:", listingMetadata);
+    console.log('Updated metadata:', listingMetadata);
   }, [listingMetadata]);
   
   useEffect(() => {
-    console.log("Updated all info:", listingInfo);
+    console.log('Updated all info:', listingInfo);
   }, [listingInfo]);
 
   const handleSubmission = async () => {
     if (!listingInfo.title || !listingInfo.price) {
-      // TODO: usability -> instead of popup -> highlight empty field with error
-      return setShowErrorPopup("Please fill out the whole form");
+      return setShowErrorPopup('Please fill out the whole form');
     }
 
-    if (!listingAddress.country||!listingAddress.postcode||!listingAddress.state||!listingAddress.streetAddress||! listingAddress.suburb) {
-      return setShowErrorPopup("Please enter all the address information");
+    if (!listingAddress.country || !listingAddress.postcode || !listingAddress.state || !listingAddress.streetAddress || !listingAddress.suburb) {
+      return setShowErrorPopup('Please enter all the address information');
     }
 
-    if (!listingMetadata.bathroomCount||!listingMetadata.propertyType||!listingMetadata.bedroomCount) {
-      return setShowErrorPopup("Please enter all the information about the property");
+    if (!listingMetadata.bathroomCount || !listingMetadata.propertyType || !listingMetadata.bedroomCount) {
+      return setShowErrorPopup('Please enter all the property information');
     }
 
     const bedNum = Number(listingMetadata.bedroomCount);
     
     if (bedNum > 0 && listingMetadata.bedrooms.length != bedNum) {
-      return setShowErrorPopup("Please enter the bedroom information");
+      return setShowErrorPopup('Please enter the bedroom information');
     }
     
     const priceNum = Number(listingInfo.price);
     const bathNum = Number(listingMetadata.bathroomCount);
     
     if (![priceNum, bathNum, bedNum].every(Number.isFinite)) {
-      return setShowErrorPopup("Please insert a number");
+      return setShowErrorPopup('Please insert a number');
     }
     
     let thumbnail = listingInfo.thumbnail;
 
-    if (thumbnail && thumbnailType === "youtube" && !thumbnail.startsWith("https://www.youtube.com/")) {
-      return setShowErrorPopup("Invalid YouTube link");
+    if (thumbnail && thumbnailType === 'youtube' && !thumbnail.startsWith('https://www.youtube.com/')) {
+      return setShowErrorPopup('Invalid YouTube link');
     };
     
     if (!thumbnail) thumbnail = DEFAULT_IMAGE;
@@ -125,9 +121,9 @@ function CreateListing({ token }) {
       metadata: listingMetadata,
       price: parseInt(listingInfo.price, 10),
       thumbnail: thumbnail
-    }
+    };
 
-    console.log("Listing data: ",body)
+    console.log('Listing data: ',body)
 
     postListing(body, token);
   }
@@ -139,7 +135,7 @@ function CreateListing({ token }) {
       <Form>
         <ListingForm getters={getters} setters={setters} />
         <Button 
-          variant="contained"
+          variant='contained'
           onClick={handleSubmission}
         >Submit</Button>
       </Form>
